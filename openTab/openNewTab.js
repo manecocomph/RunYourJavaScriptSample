73c440051f739b6196a4e377cac90e376a1ed5f1
Eric <manecocomph@163.com>
2018-01-16 19:49:21 -0700

    Create openNewTab.js

---
+++ openTab/openNewTab.js
@@ -0,0 +1,17 @@
+//open new tab with URL: http://www.google.com
+// see https://developer.chrome.com/extensions/tabs#method-create for more details
+var googleUrl = "www.google.com";
+chrome.tabs.create({"url": googleUrl});  
+
+//open new tab and run some scripts
+console.log("running script after document is ready!");
+var script = `
+  alert("I am going to search something!");
+  document.getElementById("lst-ib").value = "tianxiaohui.com";
+  document.querySelector("input[name='btnK'][type='submit']").click();
+  `;
+ 
+chrome.tabs.create({ url: googleUrl}, function(tab){
+    console.log(tab.id);
+    chrome.tabs.executeScript(tab.id,{code:script});
+  });  
